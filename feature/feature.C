9aa79c846afc5986fdb940859e89f80398b2615e
0xdb2 <dimitri.belov@gmx.net>
2016-06-19 16:10:07 +0200

    refactored

--- feature/feature.C
+++ feature/feature.C
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 
 class Feature
 {
@@ -10,55 +11,97 @@ public:
     ~Feature()
     {
         if (points)
-            delete points;
+        {
+            delete [] points;
+        }
     }
 
     bool isValid()
     {
-        return type != eUnknown;
+        return (type != eUnknown && points);
     }
 
     bool read(FILE* file)
     {
-        if (fread(&type, sizeof(FeatureType), 1, file) != sizeof(FeatureType))
-            return false;
+        int readItems = fread(&type, sizeof(FeatureType), 1, file);
+        if (readItems != 1) return false;
+
         short n = 0;
         switch (type)
         {
-        case eCircle: n = 3; break;
-        case eTriangle: n = 6; break;
-        case eSquare: n = 8; break;
-        default: type = eUnknown; return false;
+            case eCircle: n = 3; break;
+            case eTriangle: n = 6; break;
+            case eSquare: n = 8; break;
+            default: type = eUnknown; return false;
         }
+
         points = new double[n];
-        if (!points)
-            return false;
-        return fread(&points, sizeof(double), n, file) == n*sizeof(double);
+        if (!points) return false;
+
+        readItems = fread(points, sizeof(double), n, file);
+        return (readItems == n);
     }
+
     void draw()
     {
         switch (type)
         {
-        case eCircle: drawCircle(points[0], points[1], points[2]); break;
-        case eTriangle: drawPolygon(points, 6); break;
-        case eSquare: drawPolygon(points, 8); break;
+            case eCircle: drawCircle(points[0], points[1], points[2]); break;
+            case eTriangle: drawPolygon(points, 6); break;
+            case eSquare: drawPolygon(points, 8); break;
         }
     }
 
-protected:
-    void drawCircle(double centerX, double centerY, double radius);
-    void drawPolygon(double* points, int size);
+private:
 
     double* points;
     FeatureType type;
+
+    void drawCircle(double centerX, double centerY, double radius)
+    {
+        std::cout << "Feature::drawCircle points=" << centerX << "/" << centerY << "/" <<  radius << std::endl;
+    }
+
+    void drawPolygon(double* points, int size)
+    {
+        std::cout << "Feature::drawPolygon points=";
+        for (int i=0; i<size; i++) std::cout << points[i] << (i != size-1?"/":"");
+        std::cout << std::endl;
+    }
+
 };
+
+bool writeTestData(Feature::FeatureType ft)
+{
+        FILE* file = fopen("features.dat", "w");
+        if (! file) return false;
+
+        double points[8] = { 3.0, 4.0, 5.0, 6.0, 8.0, 1.0, 9.0, 7.0 };
+        int wroteItems;
+
+        wroteItems = fwrite(&ft, sizeof(Feature::FeatureType), 1, file);
+        wroteItems =+ fwrite(&points, sizeof(points), 1, file);
+
+        fclose(file);
+        return (wroteItems == 2);
+}
 
 int main(int argc, char* argv[])
 {
+    std::cout << "::main start" << std::endl;
     Feature feature;
+
+    writeTestData(Feature::eSquare);
+
     FILE* file = fopen("features.dat", "r");
+    if (! file) return 2;
+
     feature.read(file);
-    if (!feature.isValid())
-        return 1;
+    if (!feature.isValid()) return 1;
+
+    feature.draw();
+    std::cout << "::main end" << std::endl;
+
     return 0;
 }
+
